323eee294c09362c6296ed927830ca90be1713e4
Jhenii01 <jhenifer.msantos@gmail.com>
2018-08-21 21:59:55 -0300

    Adicionado cor as celulas.
    Signed-off-by: Jhenii01 <jhenifer.msantos@gmail.com>

--- js/btn-AdicionarPonteiro.js
+++ js/btn-AdicionarPonteiro.js
@@ -1,46 +1,66 @@
-function add(dado,next){
+function add(dado, next) {
     var numeroPonteiro = $("#qtd-ponteiro").val();
 
-         numeroPonteiro++;
-        
-        if (numeroPonteiro < 5) {
-            $("#qtd-ponteiro").val(numeroPonteiro);
-            $("#area-ponteiro")
-                .append('<div class="celula">' +
-                    '<label>' + numeroPonteiro + '</label>' +
-                    '<div class="row celula-display">' +
-                    '<div class="row">' +
-                    '<input class="col-md-2 float-left display" value="'+dado+'" />' +
-                    '<input class="col-md-2 float-left display" value="'+next+'" />' +
-                    '</div>' +
-                    '<div class="row">' +
-                    '<a href="#" class="btn-navegacao" id="btn-before-' + numeroPonteiro + '">' +
-                    '<i class="material-icons">' +
-                    'navigate_before' +
-                    '</i>' +
-                    '</a>' +
-                    '<a href="#" class="btn-navegacao" id="btn-next-' + numeroPonteiro + '">' +
-                    '<i class="material-icons">' +
-                    'navigate_next' +
-                    '</i>' +
-                    '</a>' +
-                    '</div>' +
-                    '</div>' +
-                    '</div>');
-        }else{
-            alert("Numero máximo de ponteiros atingido");
-        } 
+    numeroPonteiro++;
+
+    if (numeroPonteiro < 5) {
+        $("#qtd-ponteiro").val(numeroPonteiro);
+        $("#area-ponteiro")
+            .append('<div class="celula">' +
+                '<label>' + numeroPonteiro + '</label>' +
+                '<div class="row celula-display" >' +
+                '<div class="row" >' +
+                '<input class="col-md-2 float-left display input-' + numeroPonteiro + '" value="' + dado + '" />' +
+                '<input class="col-md-2 float-left display input-' + numeroPonteiro + '" value="' + next + '" />' +
+                '</div>' +
+                '<div class="row">' +
+                '<a href="#" class="btn-navegacao" id="btn-before-' + numeroPonteiro + '">' +
+                '<i class="material-icons">' +
+                'navigate_before' +
+                '</i>' +
+                '</a>' +
+                '<a href="#" class="btn-navegacao" id="btn-next-' + numeroPonteiro + '">' +
+                '<i class="material-icons">' +
+                'navigate_next' +
+                '</i>' +
+                '</a>' +
+                '</div>' +
+                '</div>' +
+                '</div>');
+        let cor = gera_cor();
+        $(".input-" + numeroPonteiro).css("background-color", cor);
+        return new Array({ number: numeroPonteiro, cor: cor });
+    } else {
+        alert("Numero máximo de ponteiros atingido");
+    }
 }
 
+// gera uma cor aleatória em hexadecimal
+function gera_cor() {
+    let cor;
+    
+    do {
+        let hexadecimais = '0123456789ABCDEF';
+        cor = '#';
+        // Pega um número aleatório no array acima
+        for (let i = 0; i < 6; i++) {
+            //E concatena à variável cor
+            cor += hexadecimais[Math.floor(Math.random() * 16)];
+        }
+        
+    } while (cores.includes(cor))
+    cores.push(cor);
+    return cor;
+}
 
 $(document).ready(function () {
 
-    $("#btn-next").click(function (){
-        var a = $( this ).parent("#endereco").siblings().prevObject.prevObject[0].nodeName;
-        $("#pluzze-"+a).hide();
-        console.log( $( this ).parent("#next-endereco").siblings()
-        
+    $("#btn-next").click(function () {
+        var a = $(this).parent("#endereco").siblings().prevObject.prevObject[0].nodeName;
+        $("#pluzze-" + a).hide();
+        console.log($(this).parent("#next-endereco").siblings()
+
         );
     })
-    
+
 });
--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 var list = [
-    { dado: '12', next: 'D' },
-    { dado: '45', next: 'F' },
-    { dado: '32', next: 'A' },
-    { dado: '28', next: 'E' },
-    { dado: '36', next: 'C' },
-    { dado: '68', next: 'B' },
+    { dado: '12', next: 'F', end: 'D' },/**Começo em D */
+    { dado: '45', next: 'A', end: 'F' },
+    { dado: '32', next: 'E', end: 'A' },
+    { dado: '28', next: 'C', end: 'E' },
+    { dado: '36', next: 'B', end: 'C' },
+    { dado: '68', next: 'NULL', end: 'B' },
 
 ];
-console.log(list);
+
+var ponteiros = new Array();
+var cores = new Array();
 
 function onLoad() {
 
@@ -15,12 +17,37 @@ function onLoad() {
         var imported = document.createElement('script');
         imported.src = 'btn-AdicionarPonteiro.js';
         document.getElementsByTagName('head')[0].appendChild(script);
-    })
+    });
+
+    posicionarLista();
     $("#head-dado").val(list[0].dado);
     $("#head-next").val(list[0].next);
+
+    let cor = gera_cor();
+    $(".input-head").css("background-color", cor);
+    colorirPluzze(cor,list[0].end);
 }
 
-function onClick(){
-    add(list[0].dado,list[0].next);
+function posicionarLista() {
+    list.forEach(e => {
+        $("#dado-" + e.end).text(e.dado);
+        $("#next-" + e.end).text(e.next);
+        $("#pluzze-" + e.end).hide();
+    });
+
+    $("#pluzze-" + list[0].end).show();
+
+}
+
+function colorirPluzze(cor, end) {
+
+    $("#dado-" + end).css("background-color", cor);
+    $("#next-" + end).css("background-color", cor);
+}
+
+function onClick() {
+    var ponteiro = add(list[0].dado, list[0].next);
+    ponteiros.push(ponteiro);
+    colorirPluzze(ponteiro[0].cor, list[0].end);
 }
 
