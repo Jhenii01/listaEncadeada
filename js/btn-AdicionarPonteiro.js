32a7ca07525fe9cff4daca7485abb406e7533d78
Jhenii01 <jhenifer.msantos@gmail.com>
2018-08-27 22:08:31 -0300

    Desenvolvidoa navegação dos ponteiros
    - Relacionado os ponteiros com os pluzze por meio de cores
    - Criado a paleta de cores
    - Definido nomes aos ponteiros
    - Desenvolvida a navegação dos ponteiros
    
    Signed-off-by: Jhenii01 <jhenifer.msantos@gmail.com>

--- js/btn-AdicionarPonteiro.js
+++ js/btn-AdicionarPonteiro.js
@@ -1,66 +1,172 @@
-function add(dado, next) {
-    var numeroPonteiro = $("#qtd-ponteiro").val();
+let nomePonteiro = ['p','q','r','s','t'];
+let nomeUsado =  ['head'];
+var ponteiros = new Array();
+var list = [
+    { dado: '12', next: 'F', end: 'D' },/**Começo em D */
+    { dado: '45', next: 'A', end: 'F' },
+    { dado: '32', next: 'E', end: 'A' },
+    { dado: '28', next: 'C', end: 'E' },
+    { dado: '36', next: 'B', end: 'C' },
+    { dado: '68', next: 'NULL', end: 'B' },
 
+];
+
+
+/**
+ * Exibe todos os pluzze que possuem ponteiro que apontam para eles
+ */
+function exibeMemoria(){
+    nomeUsado.forEach(e =>{
+        var memoria = $( "input[value='"+e+"']" ).next().val();
+        let btn =  $( "input[value='"+e+"']" ).next().next();
+        let cor = ponteiros.filter(p => p.nome == e)[0].cor;
+        colorirPluzze(cor,memoria);
+        show(memoria);
+    })
+    
+}
+
+/**
+ * Função seleciona um nome para o ponteiro a partir do array global nomePonteiro
+ * e adiciona ao array nomeUsado
+ * 
+ * @return nome para um ponteiro
+ */
+function escolheNomePonteiro(){
+    
+    let nome = null;
+    do{
+        let index = Math.floor(Math.random() * 5);
+        nome = nomePonteiro[index];
+    }while(nomeUsado.includes(nome));
+    
+    nomeUsado.push(nome);
+    return nome;
+}
+
+/**
+ * Esconde o dado e o input que estão dentro do pluzze 
+ * 
+ * @param {*} endHide : endereço do pluzze que deve ser escondido
+ */
+function hide(endHide){
+    $("#dado-"+endHide).hide();
+    $("#next-"+endHide).hide();
+}
+
+/**
+ * Exibe o dado e o input que estão dentro do pluzze
+ * 
+ * @param {*} endShow : endereço do pluzze que deve ser exbido
+ */
+function show(endShow){
+    $("#dado-"+endShow).show();
+    $("#next-"+endShow).show();
+}
+
+
+/**
+ * Faz a navegação do ponteiro Head 
+ * 
+ * @return endereco que deve ser exibido
+ */
+function navHead(){
+    let endHide = $("#head-end").val().toUpperCase(); /*Valor que está no input do ponteiro */
+    let endShow = $("#next-"+endHide).val(); /* Valor que está no input do pluzze */
+    hide(endHide);
+    show(endShow);
+    $("#head-end").val(endShow);    
+    exibeMemoria();
+    return endShow;
+}
+
+/**
+ * Faz a nevegação dos ponteiros adicionados depois
+ * 
+ * @param {*} btn : botão que foi ativado para ir para o próximo nó
+ */
+function navNext(btn){
+    let endHide = $( btn ).prev("input").val().toUpperCase();/*Valor que está no input do ponteiro */
+    let endShow = $("#next-"+endHide).val(); /* Valor que está no input do pluzze */
+
+    hide(endHide);
+    show(endShow);
+    $( btn ).prev("input").val(endShow);
+
+    colorirAposNavegacao(btn,endShow);
+    exibeMemoria();
+    
+}
+
+/**
+ * Tinge os campos do pluzze após a navegação
+ * 
+ * @param {*} btn : botão do ponteiro que o usuario selecionou
+ * @param {*} enderecoPluzze : endereco do pluzze que deve ser preenchido com cor
+ */
+function colorirAposNavegacao(btn, enderecoPluzze){
+    var nome = $( btn ).prev("input").prev("input").val();
+    let cor = procurarCor(nome);
+    colorirPluzze(cor, enderecoPluzze);
+}
+
+/**
+ * Adiciona um ponteiro a tela
+ * 
+ * @param {*} endereco : endereço em que o ponteiro está apontando
+ */
+function add(endereco) {
+    var numeroPonteiro = $("#qtd-ponteiro").val();
+    let nome = escolheNomePonteiro();
     numeroPonteiro++;
 
     if (numeroPonteiro < 5) {
         $("#qtd-ponteiro").val(numeroPonteiro);
         $("#area-ponteiro")
             .append('<div class="celula">' +
-                '<label>' + numeroPonteiro + '</label>' +
+                '<label>' + nome.toUpperCase() + '</label>' +
                 '<div class="row celula-display" >' +
-                '<div class="row" >' +
-                '<input class="col-md-2 float-left display input-' + numeroPonteiro + '" value="' + dado + '" />' +
-                '<input class="col-md-2 float-left display input-' + numeroPonteiro + '" value="' + next + '" />' +
-                '</div>' +
-                '<div class="row">' +
-                '<a href="#" class="btn-navegacao" id="btn-before-' + numeroPonteiro + '">' +
-                '<i class="material-icons">' +
-                'navigate_before' +
-                '</i>' +
-                '</a>' +
-                '<a href="#" class="btn-navegacao" id="btn-next-' + numeroPonteiro + '">' +
+                '<input type="hidden" value='+nome+'>'+
+                '<input class="col-md-2 float-left display input-' + nome + '" value="' + endereco + '" />' +
+                '<a href="#" class="btn-navegacao" onclick="navNext(this)" >' +
                 '<i class="material-icons">' +
                 'navigate_next' +
                 '</i>' +
                 '</a>' +
                 '</div>' +
-                '</div>' +
                 '</div>');
         let cor = gera_cor();
-        $(".input-" + numeroPonteiro).css("background-color", cor);
-        return new Array({ number: numeroPonteiro, cor: cor });
+        $(".input-" + nome).css("background-color", cor);
+        show(endereco);
+        return { nome: nome, cor: cor };
     } else {
         alert("Numero máximo de ponteiros atingido");
+        return null;
     }
 }
 
-// gera uma cor aleatória em hexadecimal
+/**
+ * Função seleciona um cor a partir de array com o auxilio de um random
+ * 
+ * @return uma cor em hexadecinal
+ */
 function gera_cor() {
-    let cor;
+    let resultado;
     
     do {
-        let hexadecimais = '0123456789ABCDEF';
-        cor = '#';
-        // Pega um número aleatório no array acima
-        for (let i = 0; i < 6; i++) {
-            //E concatena à variável cor
-            cor += hexadecimais[Math.floor(Math.random() * 16)];
-        }
+    let cor = ['#4dd880','#fad6ae','#b1e4d3','#823e9b','#ece172','#8be17e','#ffc107',
+            '#26a993','#d448a1','#02f46e'];
+        let index = Math.floor(Math.random() * 10);
+        resultado = cor[index];
         
-    } while (cores.includes(cor))
-    cores.push(cor);
-    return cor;
+    } while (cores.includes(resultado))
+    cores.push(resultado);
+    return resultado;
 }
 
-$(document).ready(function () {
 
-    $("#btn-next").click(function () {
-        var a = $(this).parent("#endereco").siblings().prevObject.prevObject[0].nodeName;
-        $("#pluzze-" + a).hide();
-        console.log($(this).parent("#next-endereco").siblings()
+/*Menu lateral voce modifica quem aparece 
+na imagem vc modifica pra quem aponta
 
-        );
-    })
-
-});
+pesquisa qualitativa
+*/
--- js/main.js
+++ js/main.js
@@ -1,53 +1,101 @@
-var list = [
-    { dado: '12', next: 'F', end: 'D' },/**Começo em D */
-    { dado: '45', next: 'A', end: 'F' },
-    { dado: '32', next: 'E', end: 'A' },
-    { dado: '28', next: 'C', end: 'E' },
-    { dado: '36', next: 'B', end: 'C' },
-    { dado: '68', next: 'NULL', end: 'B' },
-
-];
-
-var ponteiros = new Array();
 var cores = new Array();
 
+var endAnt = null;
+
+/**
+ * Função inicial que faz o carregamento inical das informações
+ */
 function onLoad() {
 
     (function () {
         var imported = document.createElement('script');
         imported.src = 'btn-AdicionarPonteiro.js';
         document.getElementsByTagName('head')[0].appendChild(script);
+        
     });
 
     posicionarLista();
-    $("#head-dado").val(list[0].dado);
-    $("#head-next").val(list[0].next);
+    $("#head-end").val(list[0].end);
 
     let cor = gera_cor();
     $(".input-head").css("background-color", cor);
-    colorirPluzze(cor,list[0].end);
+
+    ponteiros.push({ nome: "head", cor: cor }); 
+       
+    colorirPluzze(cor, list[0].end);
 }
 
+/**
+ * Função posiciona todos os pluzze na tela
+ */
 function posicionarLista() {
     list.forEach(e => {
         $("#dado-" + e.end).text(e.dado);
-        $("#next-" + e.end).text(e.next);
-        $("#pluzze-" + e.end).hide();
+        $("#next-" + e.end).val(e.next);
+        hide(e.end);
     });
 
-    $("#pluzze-" + list[0].end).show();
+    show(list[0].end);
 
 }
 
+/**
+ * Função controla a adição do poteiro e o prenchimento de cor do mesmo 
+ */
+function onClick() {
+    let ponteiro = add(list[0].end);
+    if(ponteiro != null){
+        ponteiros.push(ponteiro);
+        colorirPluzze(ponteiro.cor, list[0].end);
+    }
+}
+
+/*Coloca cor no pluzze */
+/**
+ * Preenche o pluzze com a cor passada por parametro
+ * 
+ * @param {*} cor : cor que será usada no preenchimento
+ * @param {*} end : endereço do pluzze que será preenchido
+ */
 function colorirPluzze(cor, end) {
 
     $("#dado-" + end).css("background-color", cor);
     $("#next-" + end).css("background-color", cor);
 }
 
-function onClick() {
-    var ponteiro = add(list[0].dado, list[0].next);
-    ponteiros.push(ponteiro);
-    colorirPluzze(ponteiro[0].cor, list[0].end);
+/**
+ * Retorna a cor referente ao ponteiro passado por parametro
+ * 
+ * @param {*} nome : nome do ponteiro que está buscando a cor
+ */
+function procurarCor(nome){
+    return ponteiros.filter(l=> l.nome == nome)[0].cor;
 }
 
+
+$(document).ready(function () {
+
+    $("#head").click(function () {
+        let enderecoPluzze = navHead();
+        
+        var nome = $( this ).prev("input").prev("input").val();
+
+        let cor = procurarCor(nome);
+        colorirPluzze(cor, enderecoPluzze);
+    })
+
+    $("#head-end").focusin(function () {
+        endAnt = $(".input-head").val().toUpperCase();
+    });
+
+    $("#head-end").focusout(function () {
+        
+        let  endAtual = $(".input-head").val().toUpperCase();
+        
+        if(endAnt != endAtual){
+            $("#pluzze-" + endAtual).show();
+            $("#pluzze-" + endAnt).hide();
+        }
+
+    });
+});
